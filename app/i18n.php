add620a89fa91de1de0d48571a375a5fd8276b5d
remzoospb <remzoo.ru@gmail.com>
2026-03-24 11:47:11 +0300

    new

---
+++ app/i18n.php
@@ -0,0 +1,408 @@
+<?php
+
+$i = [
+    'warp'             => [
+        'en' => 'Warp',
+        'ru' => 'Warp',
+    ],
+    'wg_title'             => [
+        'en' => 'Wireguard',
+        'ru' => 'Wireguard',
+    ],
+    'amnezia'             => [
+        'en' => 'Amnezia',
+        'ru' => 'Amnezia',
+    ],
+    'sh_title'             => [
+        'en' => 'Shadowsocks',
+        'ru' => 'Shadowsocks',
+    ],
+    'ad_title'             => [
+        'en' => 'AdGuard',
+        'ru' => 'AdGuard',
+    ],
+    'config'               => [
+        'en' => 'Settings',
+        'ru' => 'настройки',
+    ],
+    'pac'                  => [
+        'en' => 'PAC',
+        'ru' => 'PAC',
+    ],
+    'chat'                 => [
+        'en' => 'chat',
+        'ru' => 'чат поддержки',
+    ],
+    'back'                 => [
+        'en' => 'back',
+        'ru' => 'назад',
+    ],
+    'import'               => [
+        'en' => 'restore',
+        'ru' => 'восстановить',
+    ],
+    'export'               => [
+        'en' => 'backup',
+        'ru' => 'сохранить',
+    ],
+    'delete'               => [
+        'en' => 'delete',
+        'ru' => 'удалить',
+    ],
+    'add'                  => [
+        'en' => 'add',
+        'ru' => 'добавить',
+    ],
+    'donate'               => [
+        'en' => 'donate',
+        'ru' => 'донат',
+    ],
+    'admin'                => [
+        'en' => 'admin',
+        'ru' => 'админа',
+    ],
+    'menu'                 => [
+        'en' => 'Menu',
+        'ru' => 'Меню',
+    ],
+    'update status'        => [
+        'en' => 'update status',
+        'ru' => 'обновить статус',
+    ],
+    'add peer'             => [
+        'en' => 'add peer',
+        'ru' => 'добавить клиента',
+    ],
+    'all traffic'          => [
+        'en' => 'all traffic',
+        'ru' => 'весь трафик',
+    ],
+    'subnet'               => [
+        'en' => 'subnet',
+        'ru' => 'подсеть',
+    ],
+    'proxy ip'             => [
+        'en' => 'proxy ip',
+        'ru' => 'айпи прокси',
+    ],
+    'rename'               => [
+        'en' => 'rename',
+        'ru' => 'переименовать',
+    ],
+    'show QR'              => [
+        'en' => 'show QR',
+        'ru' => 'показать QR',
+    ],
+    'download config'      => [
+        'en' => 'download config',
+        'ru' => 'скачать настройки',
+    ],
+    'install domain'       => [
+        'en' => 'install domain',
+        'ru' => 'установить домен',
+    ],
+    'renew SSL'            => [
+        'en' => 'renew SSL',
+        'ru' => 'обновить SSL',
+    ],
+    'delete SSL'           => [
+        'en' => 'delete SSL',
+        'ru' => 'удалить SSL',
+    ],
+    'Letsencrypt SSL'      => [
+        'en' => 'Letsencrypt SSL',
+        'ru' => 'Letsencrypt SSL',
+    ],
+    'Self SSL'             => [
+        'en' => 'Self SSL',
+        'ru' => 'Собственный SSL',
+    ],
+    'change password'      => [
+        'en' => 'change password',
+        'ru' => 'изменить пароль',
+    ],
+    'reset settings'       => [
+        'en' => 'reset settings',
+        'ru' => 'сбросить настройки',
+    ],
+    'add upstream'         => [
+        'en' => 'add upstream',
+        'ru' => 'добавить DNS',
+    ],
+    'check DNS'            => [
+        'en' => 'check DNS',
+        'ru' => 'проверить DNS',
+    ],
+    'update'               => [
+        'en' => 'update',
+        'ru' => 'обновить',
+    ],
+    'self list'            => [
+        'en' => 'self list',
+        'ru' => 'собственный список',
+    ],
+    'reverse list'         => [
+        'en' => 'reverse list',
+        'ru' => 'обратный список',
+    ],
+    'subzones'             => [
+        'en' => 'subzones',
+        'ru' => 'поддомены',
+    ],
+    'check url'            => [
+        'en' => 'check url',
+        'ru' => 'проверить домен',
+    ],
+    'blacklist exclude'    => [
+        'en' => 'blacklist exclude',
+        'ru' => 'исключения из списка ркн',
+    ],
+    'lang'                 => [
+        'en' => 'language',
+        'ru' => 'язык интерфейса',
+    ],
+    'adguard web'          => [
+        'en' => 'web interface',
+        'ru' => 'вебморда адгварда',
+    ],
+    'domain explain'       => [
+        'en' => 'Some clients require a valid certificate when connecting, such as windows 11 DoH or ShadowSocks Android (PAC url), this requires a domain',
+        'ru' => 'Некоторым клиентам требуется валидный сертификат при подключении, например Windows 11 DoH или ShadowSocks Android (URL-адрес PAC), для этого требуется домен',
+    ],
+    'self list explain'    => [
+        'en' => ' - domains that will work through a proxy, all others directly',
+        'ru' => ' - домены, которые будут работать через прокси, все остальные напрямую',
+    ],
+    'reverse list explain' => [
+        'en' => ' - domains that will work directly, all others through a proxy',
+        'ru' => ' - домены которые будут работать напрямую, все остальные через прокси',
+    ],
+    'timer'                => [
+        'en' => 'set timer',
+        'ru' => 'установить время действия',
+    ],
+    'reset nginx'          => [
+        'en' => 'reset nginx',
+        'ru' => 'сбросить nginx',
+    ],
+    'set internal dns'     => [
+        'en' => 'set internal dns',
+        'ru' => 'установить внутренний dns',
+    ],
+    'delete internal dns'  => [
+        'en' => 'delete internal dns',
+        'ru' => 'удалить внутренний dns',
+    ],
+    'hwid limit' => [
+        'en' => 'HWID limit',
+        'ru' => 'HWID лимит',
+    ],
+    'hwid devices' => [
+        'en' => 'HWID devices',
+        'ru' => 'Устройства HWID',
+    ],
+    'set hwid devices count' => [
+        'en' => 'set HWID devices count',
+        'ru' => 'установить количество HWID устройств',
+    ],
+    'use default hwid limit' => [
+        'en' => 'use default limit',
+        'ru' => 'использовать лимит по умолчанию',
+    ],
+    'no devices' => [
+        'en' => 'no devices',
+        'ru' => 'нет устройств',
+    ],
+    'hwid notice' => [
+        'en' => 'HWID limit works only when subscription is refreshed or added',
+        'ru' => 'HWID лимит срабатывает только в момент обновления и добавления подписки',
+    ],
+    'on'  => [
+        'en' => '🟢',
+        'ru' => '🟢',
+    ],
+    'off'  => [
+        'en' => '🔴',
+        'ru' => '🔴',
+    ],
+    'torrent'  => [
+        'en' => 'torrents',
+        'ru' => 'торренты',
+    ],
+    'blinkmenuon'  => [
+        'en' => 'blinkmenu on',
+        'ru' => 'прыгающее меню вкл',
+    ],
+    'blinkmenuoff'  => [
+        'en' => 'blinkmenu off',
+        'ru' => 'прыгающее меню выкл',
+    ],
+    'listSubnet'  => [
+        'en' => 'listSubnet',
+        'ru' => 'список подсетей',
+    ],
+    'AllowedIPs'  => [
+        'en' => 'AllowedIPs',
+        'ru' => 'AllowedIPs',
+    ],
+    'calc'  => [
+        'en' => 'calc CIDR',
+        'ru' => 'calc CIDR',
+    ],
+    'mtproto'  => [
+        'en' => 'MTProto',
+        'ru' => 'MTProto',
+    ],
+    'generateSecret'  => [
+        'en' => 'generateSecret',
+        'ru' => 'сгенерировать ключ',
+    ],
+    'setSecret'  => [
+        'en' => 'setSecret',
+        'ru' => 'установить свой ключ',
+    ],
+    'defaultDNS'  => [
+        'en' => 'defaultDNS',
+        'ru' => 'дефолтный днс',
+    ],
+    'defaultMTU'  => [
+        'en' => 'defaultMTU',
+        'ru' => 'дефолтный MTU',
+    ],
+    'debug'  => [
+        'en' => 'debug mode',
+        'ru' => 'режим отладки',
+    ],
+    'backup'  => [
+        'en' => 'auto backup',
+        'ru' => 'автобэкап',
+    ],
+    'logs'  => [
+        'en' => 'logs',
+        'ru' => 'логи',
+    ],
+    'clear'  => [
+        'en' => 'clear',
+        'ru' => 'очистить',
+    ],
+    'xray'  => [
+        'en' => 'Vless',
+        'ru' => 'Vless',
+    ],
+    'clash'  => [
+        'en' => 'mihomo',
+        'ru' => 'mihomo',
+    ],
+    'geodb'  => [
+        'en' => 'GeoIp/GeoSite',
+        'ru' => 'GeoIp/GeoSite',
+    ],
+    'changeFakeDomain'  => [
+        'en' => 'changeFakeDomain',
+        'ru' => 'фейковый домен',
+    ],
+    'selfFakeDomain'  => [
+        'en' => 'steal from yourself',
+        'ru' => 'steal from yourself',
+    ],
+    'page'  => [
+        'en' => 'pagination',
+        'ru' => 'пагинация',
+    ],
+    'exchange'  => [
+        'en' => 'client isolation',
+        'ru' => 'изоляция клиентов',
+    ],
+    'ocserv'  => [
+        'en' => 'OpenConnect',
+        'ru' => 'OpenConnect',
+    ],
+    'naive'  => [
+        'en' => 'NaiveProxy',
+        'ru' => 'NaiveProxy',
+    ],
+    'change login'  => [
+        'en' => 'change login',
+        'ru' => 'изменить логин',
+    ],
+    'change secret'  => [
+        'en' => 'change secret',
+        'ru' => 'секретное слово',
+    ],
+    'dns'  => [
+        'en' => 'dns',
+        'ru' => 'днс',
+    ],
+    'mirror'  => [
+        'en' => 'mirror',
+        'ru' => 'зеркало',
+    ],
+    'download'  => [
+        'en' => 'download',
+        'ru' => 'скачать',
+    ],
+    'update bot'  => [
+        'en' => 'update bot',
+        'ru' => 'обновить бота',
+    ],
+    'third party browser'  => [
+        'en' => 'third party browser',
+        'ru' => 'сторонний браузер',
+    ],
+    'browser_notify_on'  => [
+        'en' => 'the web panel can be opened in any browser',
+        'ru' => 'веб панель может быть открыта в любом браузере',
+    ],
+    'browser_notify_off'  => [
+        'en' => 'web panel is only available from telegram',
+        'ru' => 'веб панель доступна только из телеграмма',
+    ],
+    'no updates'  => [
+        'en' => 'no updates',
+        'ru' => 'нет обновлений',
+    ],
+    'have updates'  => [
+        'en' => '🟢 have updates',
+        'ru' => '🟢 есть обновления',
+    ],
+    'fake html'  => [
+        'en' => 'fake html',
+        'ru' => 'фейк заглушка',
+    ],
+    'expose-iroutes'  => [
+        'en' => 'expose-iroutes',
+        'ru' => 'изоляция клиентов',
+    ],
+    'fill allowed clients'  => [
+        'en' => 'fill allowed clients',
+        'ru' => 'заполнить белый список клиентов',
+    ],
+    'delete allowed clients'  => [
+        'en' => 'delete allowed clients',
+        'ru' => 'очистить белый список клиентов',
+    ],
+    'success'  => [
+        'en' => 'success',
+        'ru' => 'сохранено',
+    ],
+    'error'  => [
+        'en' => 'error',
+        'ru' => 'ошибка',
+    ],
+    'save'  => [
+        'en' => 'save',
+        'ru' => 'сохранить',
+    ],
+    'copy'  => [
+        'en' => 'copy',
+        'ru' => 'копировать',
+    ],
+    'restart'  => [
+        'en' => 'restart',
+        'ru' => 'перезагрузка',
+    ],
+];
+
+if (file_exists(__DIR__ . '/i18n.override.php')) {
+    include __DIR__ . '/i18n.override.php';
+}
